cc1d5ac8f736b7ca59badbc3b999157a41828064
joel-briones-B-Low <joellbriones701@gmail.com>
2025-10-23 13:34:33 -0600

    data types

--- main.go
+++ main.go
@@ -5,3 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Learning Go!")
 }
+
+/*
+data types in Go:
+int int8 int16 int32 int64 // whole numbers | control the size
+uint uint8 uint16 uint32 uint64 // unsigned whole numbers | only positive
+float32 float64 // decimal numbers | precision control | depend on system
+bool // true or false | flag or condition
+string // sequence of characters | text
+complex64 complex128  // complex numbers | real and imaginary parts
+byte // alias for uint8 | represents a single byte of data
+rune // alias for int32 | represents a unique character (Unicode code point)
+*/
