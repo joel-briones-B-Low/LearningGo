4c74bb8c4aa589b4248838d314f4edeca5ce8fd1
joel-briones-B-Low <joellbriones701@gmail.com>
2025-10-23 15:24:21 -0600

    conditionals

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("This is the main.go file.")
+	// conditionals if else ifelse
+	edad := 20
+	/*if edad < 18 {
+		println("Eres menor de edad")
+	} else if edad >= 18 && edad < 65 {
+		println("Eres un adulto")
+	} else {
+		println("Eres un adulto mayor")
+	*/
+
+	if edad < 18 {
+		println("Eres menor de edad")
+		return
+	}
+
+	println("Eres un adulto")
+	println("Fin del programa")
+
 }
