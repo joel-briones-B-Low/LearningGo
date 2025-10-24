a5a83ce26a3800749a5bfc31b6c7d994dad505fe
joel-briones-B-Low <joellbriones701@gmail.com>
2025-10-24 15:11:06 -0600

    channels

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+func sayHello(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Hello from goroutine!"
+}
+
+func printMessage(channel <-chan string) {
+	println("Waiting to receive message from channel...")
+	mes := <-channel
+	fmt.Println(mes)
+}
 func main() {
-	fmt.Println("Hello, World!")
+
+	channel := make(chan string)
+	println("Starting goroutine...")
+	go sayHello(channel)
+	println("Waiting for message...")
+	printMessage(channel)
+	println("Message received, exiting.")
+
+	channel2 := make(chan int)
+	go func() {
+		println("Goroutine started.")
+
+		for i := 0; i < 5; i++ {
+			channel2 <- i
+		}
+		close(channel2)
+	}()
+
+	for val := range channel2 {
+		fmt.Println("Received value:", val)
+	}
 }
